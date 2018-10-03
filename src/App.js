ff883809c6cde1fa4877107d816b3769ccd7625d
Israel Adura <israeladura@gmail.com>
2018-10-03 22:32:40 +0100

    query string search

--- src/App.js
+++ src/App.js
@@ -6,16 +6,19 @@ import {
   Button,
   Header,
   Grid,
+  List,
   Segment,
-  Image
+  Image,
+  GridColumn
 } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
-
+import logo from "./logo.jpg";
 export default class extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       url: undefined,
+      urls: undefined,
       start: undefined,
       end: undefined,
       query: undefined,
@@ -24,91 +27,130 @@ export default class extends React.Component {
       isSubmitting: false
     };
   }
-  componentDidMount() {}
+
   handleDownload = () => {
     let { url, start, end, query, isSubmitting, disabled, season } = this.state;
     url = url.toString().trim();
-    query = `S0${season}E0{start}`;
+
+    query = url.match(/S0\dE\d+/);
+
     let i = start;
+    let urls = [];
+
     for (i; i < end; i++) {
-      console.log(url.replace(query, `S0${season}E0${i}`));
-      this.downloadEpisodes(url.replace(query, `S0${season}E0${i}`));
+      const fileName = url.substring(url.lastIndexOf("/") + 1, url.length);
+      const link = url.replace(query, `S0${season}E0${i}`);
+      console.log(link);
+      urls.push({ fileName, link });
     }
+
+    this.setState({ urls });
   };
 
-  downloadEpisodes(url, fileName) {
-    var link = document.createElement("a");
-    link.href = url;
-    //link.target = "_blank";
-    link.setAttribute("_target", "blank");
-    link.setAttribute("download", fileName);
-    // link.style.display = "none";
-    console.log(link);
-    // var evt = new MouseEvent("click", {
-    // 	view: window,
-    // 	bubbles: true,
-    // 	cancelable: true
-    // });
+  renderUrls = () => {
+    const { urls } = this.state;
 
-    document.body.appendChild(link);
-    //link.click();
-    //document.body.removeChild(link);
-  }
+    return (
+      <List>
+        {urls
+          ? urls.map(url => {
+              const { link, fileName } = url;
+              return (
+                <List.Item
+                  icon="linkify"
+                  key={fileName}
+                  content={<a href={link}>{fileName}</a>}
+                />
+              );
+            })
+          : null}
+      </List>
+    );
+  };
+  //auto download not yet working. to be solved
+  // downloadEpisodes(url, fileName) {
+  //   var link = document.createElement("a");
+  //   link.href = url;
+  //   //link.target = "_blank";
+  //   link.setAttribute("_target", "blank");
+  //   link.download = fileName;
+  //   link.style.display = "block";
+  //   link.innerHTML = fileName;
+
+  //   // var evt = new MouseEvent("click", {
+  //   // 	view: window,
+  //   // 	bubbles: true,
+  //   // 	cancelable: true
+  //   // });
+
+  //   document.body.appendChild(link);
+  //   // link.click();
+  //   //document.body.removeChild(link);
+  // }
 
   renderDetails = e => {
     const { url, start, end, isSubmitting, disabled, season } = this.state;
     return (
-      <Container text style={{ marginTop: "100px" }}>
+      <Container text style={{ marginTop: "100 auto" }}>
         <Header as="h1">Series Download</Header>
-        <Grid centered columns={1}>
-          <Grid.Column>
-            <Form
-              onSubmit={() => {
-                this.handleDownload();
-              }}
-            >
-              <Form.Input
-                label="download link"
-                type="text"
-                onChange={e => this.setState({ url: e.target.value })}
-                placeholder="enter url for one of the episodes here"
-              />
-              <Form.Input
-                label="enter the season?"
-                type="number"
-                onChange={e => this.setState({ season: e.target.value })}
-                placeholder="e.g 2"
-              />
-              <Form.Input
-                label="start from episode?"
-                type="number"
-                onChange={e => this.setState({ start: e.target.value })}
-                placeholder="e.g 1"
-              />
-
-              <Form.Input
-                label="to episode?"
-                type="number"
-                onChange={e => this.setState({ end: e.target.value })}
-                placeholder="e.g 10"
-              />
-
-              <Button
-                onClick={() => {
+        <Grid>
+          <Grid.Row>
+            <Grid.Column width={10}>
+              <Form
+                onSubmit={() => {
                   this.handleDownload();
                 }}
-                disabled={url && start && end && season ? false : true}
               >
-                Download
-              </Button>
-            </Form>
-          </Grid.Column>
+                <Form.Input
+                  label="download link"
+                  type="text"
+                  onChange={e => this.setState({ url: e.target.value })}
+                  placeholder="enter url for one of the episodes here"
+                />
+                <Form.Input
+                  label="enter the season?"
+                  type="number"
+                  onChange={e => this.setState({ season: e.target.value })}
+                  placeholder="e.g 2"
+                />
+                <Form.Input
+                  label="start from episode?"
+                  type="number"
+                  onChange={e => this.setState({ start: e.target.value })}
+                  placeholder="e.g 1"
+                />
+
+                <Form.Input
+                  label="to episode?"
+                  type="number"
+                  onChange={e => this.setState({ end: e.target.value })}
+                  placeholder="e.g 10"
+                />
+
+                <Button
+                  onClick={() => {
+                    this.handleDownload();
+                  }}
+                  disabled={url && start && end && season ? false : true}
+                >
+                  Download
+                </Button>
+              </Form>
+            </Grid.Column>
+
+            <Grid.Column width={4}>{this.renderUrls()}</Grid.Column>
+          </Grid.Row>
         </Grid>
       </Container>
     );
   };
 
   render() {
-    return <Container>{this.renderDetails()}</Container>;
+    return (
+      <Container>
+        <Image src={logo} />
+        {this.renderDetails()}
+      </Container>
+    );
   }
 }
